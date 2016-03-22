c134d6904d6a7ed568be640589fa41f0e09b0d33
Frank Felhoffer <beta@hackme.ca>
2016-03-21 21:39:45 -0400

    Export/Import folder additions

---
+++ ExportVMFolderPath.ps1
@@ -0,0 +1,136 @@
+function Get-VMFolderPath
+{
+ <#
+   .Synopsis
+ 
+    Get vm folder path. From Datacenter to folder that keeps the vm.
+ 
+   .Description
+ 
+    This function returns vm folder path. As a parameter it takes the 
+    current folder in which the vm resides. This function can throw
+    either 'name' or 'moref' output. Moref output can be obtained
+    using the -moref switch.
+ 
+    .Example
+ 
+    get-vm 'vm123' | get-vmfolderpath
+ 
+    Function will take folderid parameter from pipeline
+     
+   .Example
+ 
+    get-vmfolderpath (get-vm myvm123|get-view).parent
+ 
+    Function has to take as first parameter the moref of vm parent
+    folder. 
+    DC\VM\folfder2\folderX\vmvm123
+    Parameter will be the folderX moref
+     
+   .Example
+ 
+    get-vmfolderpath (get-vm myvm123|get-view).parent -moref
+ 
+    Instead of names in output, morefs will be given.
+ 
+ 
+    .Parameter folderid
+ 
+    This is the moref of the parent directory for vm.Our starting
+    point.Can be obtained in serveral ways. One way is to get it
+    by: (get-vm 'vm123'|get-view).parent  
+    or: (get-view -viewtype virtualmachine -Filter @{'name'=
+    'vm123'}).parent
+     
+   .Parameter moref
+ 
+    Add -moref when invoking function to obtain moref values
+ 
+   .Notes
+ 
+    NAME:  Get-VMFolderPath
+ 
+    AUTHOR: Grzegorz Kulikowski
+ 
+    LASTEDIT: 09/14/2012
+     
+    NOT WORKING ? #powercli @ irc.freenode.net 
+ 
+   .Link
+ 
+    https://psvmware.wordpress.com
+ 
+ #>
+ 
+param(
+[Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true)]
+[string]$folderid,
+[switch]$moref
+)
+ 
+    $folderparent = Get-View $folderid
+
+    if ($folderparent.name -ne 'vm') {
+
+        if ($moref) {
+
+            $path = $folderparent.moref.toString() + '\' + $path
+        }
+
+        else {
+
+            $path = $folderparent.name + '\' + $path
+        }
+
+        if ($folderparent.parent) {
+
+            if ($moref) {
+
+                Get-VMFolderPath $folderparent.parent.tostring() -moref
+            }
+
+            else {
+                Get-VMFolderPath $folderparent.parent.tostring()
+            }
+        }
+    }
+
+    else {
+
+        if ($moref) {
+
+            $finalPath = (Get-View $folderparent.parent).moref.tostring() + '\' + $path
+        }
+
+        else {
+
+            $finalPath = (Get-View $folderparent.parent).name.toString() + '\' + $path
+        }
+
+        return $finalPath.trim("\")
+    }
+}
+
+$outVMs = @()
+$allVMs = Get-VMHost -Name 10.80.168.105 | Get-VM
+
+foreach ($thisVM in $allVMs) {
+
+    $myVM = "" | select name,folderPath
+    $myVM.name = $thisVM.name
+    $myVM.folderPath = $thisVM | Get-VMFolderPath
+
+    $outVMs += $myVM
+}
+
+$outVMs | export-clixml $directory\VMs.xml
+
+
+
+foreach ($thisVM in $allVMs) {
+
+    $myVM = "" | select name,folderPath
+    $myVM.name = $thisVM.name
+    $thisVM | Get-VMFolderPath
+}
+
